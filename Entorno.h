25c6a802bfe6d39755a234114796940652625334
isa.ramirez <isaramirez28@javerianacali.edu.co>
2025-05-07 01:02:17 -0500

    Actualizacion entorno

--- Entorno.h
+++ Entorno.h
@@ -5,10 +5,57 @@
 #ifndef ENTORNO_H
 #define ENTORNO_H
 
+#include <iostream>
+#include <vector>
+#include <string>
 
+#include "Albo.h"
+#include "Centella.h"
+#include "Criatura.h"
+#include "Metamorfita.h"
+#include "Raiz.h"
+using namespace std;
 
 class Entorno {
+public:
+    Entorno(const string &nombre_entorno)
+        : nombreEntorno(nombre_entorno) {
+    }
+    string getNombreEntorno() {
+        return nombreEntorno;
+    }
+    void agregarCriaturaRaiz(string nombreRa, string reinoRa) {
+        criaturas.push_back(new Raiz(nombreRa, reinoRa, energia, tasaInteres));
+    }
 
+    void agregarCriaturaCentella(string nombreCent, string reinoCent) {
+        criaturas.push_back(new Centella(nombreCent, reinoCent, energia, tasaInteres));
+    }
+
+    void agregarCriaturaMetamorfita(string nombreMeta, string reinoMeta) {
+        criaturas.push_back(new Metamorfita(nombreMeta, reinoMeta, energia, tasaInteres));
+    }
+
+    void agregarCriaturaAlbo(string nombreAlb, string reinoAlb) {
+        criaturas.push_back(new Albo(nombreAlb, reinoAlb, energia, tasaInteres));
+    }
+
+    void mostrarmapa();
+
+    void guardarDatos(const char * str);
+
+    void moverCriaturas();
+
+    void atacaCriatura(string criaAtacante, string criaVictima, int poderAtaque);
+
+private:
+    string nombreEntorno;
+    vector<Criatura*> criaturas;
+    int energia = 100;
+    int filas = 10;
+    int columnas = 10;
+    int posicionCriaturaX;
+    int posicionCriaturaY;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,37 +5,67 @@ using namespace std;
 
 int main() {
     Entorno entorno("El Valle Iridiano ");
-    int opcion;
-    string reino, nombre, energía;
+    int opcion, opcionAcciones, fuerzaAtaque;
+    string energía, nombreCentella, nombreRaiz, nombreMetamorfita, nombreAlbo, atacante, victima;
+    string reinoRaiz = "Raiz";
+    string reinoCentella = "Chispa";
+    string reinoMetamorfita = "Cambio";
+    string reinoAlbo = "Chiscambio";
 
     do {
         cout << "=========MENU========" << endl;
         cout << "1. INICIAR CICLO" << endl;
-        cout << "2. HISTORIAL" << endl;
-        cout << "3. CARGAR" << endl;
-        cout << "4. SALIR DEL SISTEMA" << endl;
+        cout << "2. CARGAR" << endl;
+        cout << "3. SALIR DEL SISTEMA" << endl;
         cout << "ELIGE UNA OPCION: ";
         cin >> opcion;
         switch (opcion) {
             case 1: {
                 cout << "========INICIO DEL JUEGO==========" << endl;
+                cout << "1. Elige los nombres de rus criaturas: " << endl;
+                cout << "Raiz :";
+                cin >> nombreRaiz;
+                cout << "Centella: ";
+                cin >> nombreCentella;
+                cout << "Metamorfita: ";
+                cin >> nombreMetamorfita;
+                cout << "Albo: ";
+                cin >> nombreAlbo;
 
+                entorno.agregarCriaturaRaiz(nombreRaiz, reinoRaiz);
+                entorno.agregarCriaturaCentella(nombreCentella, reinoCentella);
+                entorno.agregarCriaturaMetamorfita(nombreMetamorfita, reinoMetamorfita);
+                entorno.agregarCriaturaAlbo(nombreAlbo, reinoAlbo);
+
+                entorno.mostrarmapa();
+
+                for (int i = 0; i < 15; ++i) {
+                    cout << "==============ESCOGE LA OPCION QUE DESEES==============" << endl;
+                    cout << "========= CICLO #" << (i + 1) << " =========" << endl;
+                    cout << "1. Mover Criaturas" << endl;
+                    cout << "2. Ataca" << endl;
+                    cout << "OPCION: ";
+                    cin >> opcionAcciones;
+                    if (opcionAcciones == 1) {
+                        entorno.moverCriaturas();
+                        entorno.mostrarmapa();
+                    } else if (opcionAcciones == 2) {
+                        cout << "INGRESA EL ATACANTE, VICTIMA Y FUERZA DE ATAQUE < 100: ";
+                        cin >> atacante >> victima >> fuerzaAtaque;
+                        entorno.atacaCriatura(atacante, victima, fuerzaAtaque);
+                    }
+                }
                 break;
             }
             case 2: {
-                cout << "========HISTORIAL DE JUEGO========" << endl;
-                break;
-            }
-            case 3: {
-                cout << "========CARGAR JUEGO========" << endl;
-            }
-            case 4: {
-                cout << "========SALIR DE JUEGO========" << endl;
+                cout << "========SALIR DEL JUEGO========" << endl;
+                entorno.guardarDatos("../valleIridiano.json");
+                cout << "DATOS GUARDADOS" << endl;
             }
             default: cout << "OPCION INVALIDA" << endl; break;
         }
 
-    } while (opcion != 4);
+    } while (opcion != 2);
 
     return 0;
 }
